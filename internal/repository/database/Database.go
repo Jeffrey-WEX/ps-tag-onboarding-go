4b13cc7479ccc2e6640bdd896963100ebb2c32ab
Jeffrey-WEX <jeffrey.siaw@wexinc.com>
2024-03-21 14:19:42 +1100

    add docker

--- internal/repository/database/Database.go
+++ internal/repository/database/Database.go
@@ -2,13 +2,13 @@ package database
 
 import (
 	"context"
+	"os"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 const (
-	url              = "mongodb://localhost:27017"
 	databaseName     = "user"
 	databaseUsername = "root"
 	databasePassword = "password"
@@ -19,15 +19,7 @@ type Database struct {
 
 func NewDatabase() *mongo.Database {
 
-	// TODO: Add authentication
-	// credential := options.Credential{
-	// 	AuthMechanism: "SCRAM-SHA-1",
-	// 	AuthSource:    "admin",
-	// 	Username:      databaseUsername,
-	// 	Password:      databasePassword,
-	// }
-
-	// client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(url).SetAuth(credential))
+	url := os.Getenv("DATABASE_URI")
 	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(url))
 
 	if err != nil {
