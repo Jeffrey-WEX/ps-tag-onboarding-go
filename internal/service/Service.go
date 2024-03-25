f0f2a1bfafcb8ff90ae66b121835226c1186a26a
Jeff-S <jeffrey.siaw@wexinc.com>
2024-03-25 12:59:38 +1100

    Revert "Feature/onboarding exercise"

--- internal/service/Service.go
+++
@@ -1,9 +0,0 @@
-package service
-
-import "github.com/Jeffrey-WEX/ps-tag-onboarding-go/internal/model"
-
-type Service interface {
-	GetAllUsers() []model.User
-	GetUserById(userId string) (*model.User, error)
-	CreateUser(newUser model.User) model.User
-}
