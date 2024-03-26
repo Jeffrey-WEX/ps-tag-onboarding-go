2259b9efb0673dc3d3266493cc7878ae8c6dfb2d
Jeffrey-WEX <jeffrey.siaw@wexinc.com>
2024-03-26 14:52:41 +1100

    add error constant file

---
+++ internal/constant/error_constant.go
@@ -0,0 +1,16 @@
+package constant
+
+const (
+	ErrorAgeMinimum         = "User does not meet minimum age requirement"
+	ErrorEmailRequired      = "User email required"
+	ErrorEmailInvalidFormat = "User email must be properly formatted"
+	ErrorNameRequired       = "User first/last names required"
+	ErrorNameAlreadyExists  = "User with the same first and last name already exists"
+	ErrorUserNotFound       = "User not found"
+	ErrorGettingUser        = "Error getting user"
+	ErrorCreatingUser       = "Error creating user"
+	ErrorFindingUser        = "Error finding user"
+	ErrorDecodingUser       = "Error decoding user"
+	ErrorRetrievingUsers    = "Error retrieving users"
+	ErrorInvalidUserObject  = "Invalid user object"
+)
