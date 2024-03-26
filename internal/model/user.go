67c4947f71df11c78528d70fdf0b8397e003a55f
Jeffrey-WEX <jeffrey.siaw@wexinc.com>
2024-03-26 14:53:17 +1100

    remove validationErrors from user struct

--- internal/model/user.go
+++ internal/model/user.go
@@ -1,10 +1,9 @@
 package model
 
 type User struct {
-	ID               string   `json:"id" bson:"_id,omitempty"`
-	FirstName        string   `json:"first_name" bson:"firstName"`
-	LastName         string   `json:"last_name" bson:"lastName"`
-	Email            string   `json:"email" bson:"email"`
-	Age              int      `json:"age" bson:"age"`
-	ValidationErrors []string `json:"-" bson:"-"`
+	ID        string `json:"id" bson:"_id,omitempty"`
+	FirstName string `json:"first_name" bson:"firstName"`
+	LastName  string `json:"last_name" bson:"lastName"`
+	Email     string `json:"email" bson:"email"`
+	Age       int    `json:"age" bson:"age"`
 }
